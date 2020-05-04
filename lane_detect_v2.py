0d094d3e382afb478489b707db26b3d56bb00c57
tpktang <42223462+tpktang@users.noreply.github.com>
2020-05-04 14:54:02 +0700

    Add files via upload

---
+++ lane_detect_v2.py
@@ -0,0 +1,108 @@
+import matplotlib.pylab as plt
+import cv2
+import numpy as np
+
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    #channel_count = img.shape[2]
+    match_mask_color = 255
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+def drow_the_lines(img, lines):
+    img = np.copy(img)
+    blank_image = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(blank_image, (x1,y1), (x2,y2), (0, 255, 0), thickness=10)
+
+    img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
+    return img
+
+# = cv2.imread('road.jpg')
+#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+def process(image):
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+    region_of_interest_vertices = [
+        (0, height*0.8),
+        (width*0.7/4, height*0.55),
+        (width*3.2/4, height*0.55),
+        (width, height*0.8),
+
+    ]
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+    canny_image = cv2.Canny(gray_image, 100, 120)
+
+    cropped_image = region_of_interest(canny_image,
+                    np.array([region_of_interest_vertices], np.int32),)
+
+    lines = cv2.HoughLinesP(cropped_image,
+                            rho=2,
+                            theta=np.pi/180,
+                            threshold=50,
+                            lines=np.array([]),
+                            minLineLength=10,
+                            maxLineGap=30)
+    image_with_lines = drow_the_lines(image, lines)
+    return image_with_lines,lines
+
+def average_slope_intercept(frame, line_segments):
+    """
+    This function combines line segments into one or two lane lines
+    If all line slopes are < 0: then we only have detected left lane
+    If all line slopes are > 0: then we only have detected right lane
+    """
+    lane_lines = []
+    if line_segments is None:
+        logging.info('No line_segment segments detected')
+        return lane_lines
+
+    height, width, _ = frame.shape
+    left_fit = []
+    right_fit = []
+
+    boundary = 0.5
+    left_region_boundary = width * (1 - boundary)  # left lane line segment should be on left 2/3 of the screen
+    right_region_boundary = width * boundary # right lane line segment should be on left 2/3 of the screen
+
+    for line_segment in line_segments:
+        for x1, y1, x2, y2 in line_segment:  
+            if x1 == x2:
+                logging.info('skipping vertical line segment (slope=inf): %s' % line_segment)
+                continue
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = fit[0]
+            intercept = fit[1]
+            
+            if slope < 0:   
+                if x1 < left_region_boundary and x2 < left_region_boundary:
+                    left_fit.append((slope, intercept))
+            else:
+                if x1 > right_region_boundary and x2 > right_region_boundary:
+                    right_fit.append((slope, intercept))
+
+    left_fit_average = np.average(left_fit, axis=0)
+    if len(left_fit) > 0:
+        lane_lines.append(make_points(frame, left_fit_average))
+
+    right_fit_average = np.average(right_fit, axis=0)
+    if len(right_fit) > 0:
+        lane_lines.append(make_points(frame, right_fit_average))
+
+    logging.debug('lane lines: %s' % lane_lines)  # [[[316, 720, 484, 432]], [[1009, 720, 718, 432]]]
+    return lane_lines
+frame = cv2.imread('D:\\tank\Year4term_2\Mechatronics_2/f0.jpg')
+frame,lines = process(frame)
+print(lines)
+lane_lines = average_slope_intercept(frame, lines)
+fin_img    = drow_the_lines(frame, lane_lines)
+cv2.imshow('frame', fin_img)
+    
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
